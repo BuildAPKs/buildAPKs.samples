ecea507f2626f31996144e84eb5845ee47511c9d
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-03-22 20:33:54 -0400

    commit 702549747 on 20210322

--- ma.bash
+++ ma.bash
@@ -41,20 +41,23 @@ _AT_ 7heaven/Bezier 975c7c667aca62fa4f4fca2d3812e15561314618
 _AT_ 7heaven/BTAndroidWebViewSelection 14a49ff27f7bcb411f5fca82579334ae8a69ea2a
 _AT_ 7heaven/TCPTest ba85c93e3b5bb211bb87d39054e3d44c5086f721
 _AT_ BullShark/AndroidTutorial f5b3ea08ab9dce1b78a67b71490dc45afe275199
+_AT_ MichaelRFairhurst/android-guitar-forms e0003c4bc1755a83243319da36cf339fa0c6af59
+_AT_ StevenByle/Android-Density-Independence-Demo 82ce51ead3ba4f1ca37256091ff61604c781b319
+([ -d StevenByle/Android-Density-Independence-Demo/StevenByle-Android-Density-Independence-Demo-82ce51e/Android\ Density\ Independence ] && mv StevenByle/Android-Density-Independence-Demo/StevenByle-Android-Density-Independence-Demo-82ce51e/Android\ Density\ Independence StevenByle/Android-Density-Independence-Demo/StevenByle-Android-Density-Independence-Demo-82ce51e/AndroidDensityIndependence) || printf "%s\\n" "Signal generated; Continuing..."
+_AT_ VelbazhdSoftwareLLC/android-vitosha-poker-odds 182a79416634154cbaf771926410e02bec849279
 _AT_ amsitlab/termuxlauncher b543e1d0e20cb36ce0c801ad3cbee0d27c055882
 _AT_ anthonycr/chromium-webview-samples b18afa96ab6215eed526c19156bf0fe6f5386ad1
 _AT_ aosp/native a527c07b8d078f7da1114b7055f65b479a148729
 _AT_ billthefarmer/draw 76802a9441976c22829dd10692232fcf7549f56f
 _AT_ burhanaras/AndroidRepo 0ee086b47413b690435642c1b16bc81b919245e2
 _AT_ burhanaras/MvApp2SD 5259ae87e34ae4e6548000251315ebf0ba7bc455
-_AT_ MichaelRFairhurst/android-guitar-forms e0003c4bc1755a83243319da36cf339fa0c6af59
 _AT_ cheezy/Androidwebviewsampleapp 5516da979ca33501b6629c1a120bfbbf9c29ac8e
 _AT_ cuongmanhvo162/Android-ImageView-hover 4c4105b96f6eb5e6c09617bf12755e03326cd9a0
 _AT_ catch/android-intent b365b0f5cb7890e71fe151036fc79bd47445b0dd
 _AT_ easytiger/dotty f6762ce2d4fa565755d53b894ae8639b1ab27342
+_AT_ enricocid/Storage-USB 6693b5ea68868e441f57d3330852b520786754cd
 _AT_ evilrobot69/rotarywear b4bd8624d77e2210f9382e6b8e43975ea5052aeb
 rm -rf  evilrobot69/rotarywear/evilrobot69-rotarywear-b4bd862/mobile/src/main
-_AT_ enricocid/Storage-USB 6693b5ea68868e441f57d3330852b520786754cd
 _AT_ foolish314159/OpenGL-2D-Engine 9cc19263678eedb80840819a89f38abc21a7cacc
 _AT_ hrj/CalendarView b0cdafe96b6ca8dbc41fde638ea5d340df484974
 _AT_ hrj/gl_test_nexus10 8e46566c843046160667db1d9b4fa22b3a4c4dfe
@@ -65,18 +68,16 @@ _AT_ matthewrkula/SecretTextView c62c38fd0b5b72cb86cdcb37c006b6f779d6e3a9
 _AT_ mikedg1/fullscreen-checker d56f3ff95a888a11d730c1e9ec5bcc332685ad1a
 _AT_ mr-dddalkilanny/tree-view-list-android 798fc10fb7a584b02533a601f0b83424e8bf5775
 _AT_ mthli/EatWhat d4d4c489afadd9e652a60f0d29408b09016b4baa
+_AT_ palazzem/SlideShow 457d835b192b8f7164d6f88f432a65059f434424
 _AT_ quaap/PhoneFoneFun 3d73e2e0af6a21e0b759c13eb1ace31cb09f06f6
 _AT_ romannurik/Android-SwipeToDismiss ecf1e51af2b1f400605b8a3786eed6944dbfbcda
 _AT_ romannurik/AndroidDesignPreview 7542db48de7124c11f39aba3909b5978c5cd6a85
 _AT_ rorist/android-alarm-button 72fd5e1f0e570387597d82bce06dcf3715283443
 _AT_ rorist/Cowsay-android c14df9e362aacb68ba99fec24d3d7dce669e043b
 _AT_ six999/android-vitosha-poker-odds b98f2052f65f81477f3f8589e9836549f45a1c5e
-_AT_ StevenByle/Android-Density-Independence-Demo 82ce51ead3ba4f1ca37256091ff61604c781b319
-([ -d StevenByle/Android-Density-Independence-Demo/StevenByle-Android-Density-Independence-Demo-82ce51e/Android\ Density\ Independence ] && mv StevenByle/Android-Density-Independence-Demo/StevenByle-Android-Density-Independence-Demo-82ce51e/Android\ Density\ Independence StevenByle/Android-Density-Independence-Demo/StevenByle-Android-Density-Independence-Demo-82ce51e/AndroidDensityIndependence) || printf "%s\\n" "Signal generated; Continuing..."
 _AT_ tobykurien/CustomControls a1659ff7defd0a9388084e8606a78c5aba4fc05d 
 _AT_ tacothedank/vanilla-headphone-detector d1211b3d7de6dc538611502fb5018b839ffe9363
- _AT_ twuster/Rubix 51be3c6c808ff95d0533ad77deac088c2af49f80
-_AT_ VelbazhdSoftwareLLC/android-vitosha-poker-odds 182a79416634154cbaf771926410e02bec849279
+_AT_ twuster/Rubix 51be3c6c808ff95d0533ad77deac088c2af49f80
 _AT_ wuman/DogFan 03684baecbb5d887cf8629646f3610e9001861c8
 _AT_ xuyisheng/XPuzzle 4ac935a9977aae69a281ad170f2e707e7b7cc717
 _AT_ xuyisheng/AndroidHeroes e301cc5313aaf2d5b4657964707c496d68c4826b
